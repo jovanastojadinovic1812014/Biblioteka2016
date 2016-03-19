6af3975752028d872c34fa21a0211329feab8689
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-19 17:04:29 +0100

    Kreirana klasa Biblioteka koja implementira metode interfejsa BibliotekaInterface.

---
+++ Biblioteka/src/Biblioteka/Biblioteka.java
@@ -0,0 +1,40 @@
+package Biblioteka;
+
+import java.util.LinkedList;
+
+import Biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+	
+	LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		LinkedList<Knjiga> nove = new LinkedList<Knjiga>();
+		
+		for (int i = 0; i < knjige.size(); i++) {
+			if(isbn.equals(isbn)){
+				nove.add(knjige.get(i));
+			}
+		}
+		
+		return null;
+	}
+
+}

726f3df48dbf8f27659187e91805154ce7ae6d95
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-19 16:31:42 +0100

    Napravljen interfejs - BibliotekaInterface.

---
+++ Biblioteka/src/Biblioteka/interfejs/BibliotekaInterface.java
@@ -0,0 +1,16 @@
+package Biblioteka.interfejs;
+
+import java.util.LinkedList;
+
+import Biblioteka.Autor;
+import Biblioteka.Knjiga;
+
+public interface BibliotekaInterface {
+	public void dodajKnjigu(Knjiga knjiga);
+
+	public void obrisiKnjigu(Knjiga knjiga);
+
+	public LinkedList<Knjiga> vratiSveKnjige();
+
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
+}

d7d8a4f4c4a3831c5d52c0fa853794621b1d3971
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-19 17:49:38 +0100

    Provera kriterijuma metode pronadjiKnjigu na master grani.

--- Biblioteka/src/Biblioteka/Biblioteka.java
+++ Biblioteka/src/Biblioteka/Biblioteka.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 import Biblioteka.interfejs.BibliotekaInterface;
 
 public class Biblioteka implements BibliotekaInterface {
-	
+
 	LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
-	
+
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		knjige.add(knjiga);
@@ -27,13 +27,17 @@ public class Biblioteka implements BibliotekaInterface {
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
 		LinkedList<Knjiga> nove = new LinkedList<Knjiga>();
-		
+
+		if (autor == null && isbn == null && naslov == null && izdavac == null) {
+			return knjige;
+		}
+
 		for (int i = 0; i < knjige.size(); i++) {
-			if(isbn.equals(isbn)){
+			if (isbn.equals(isbn)) {
 				nove.add(knjige.get(i));
 			}
 		}
-		
+
 		return null;
 	}
 
